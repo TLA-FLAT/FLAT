e063e159c9335b2a768cb809a2df561a44eb8811
Menzo <menzo.windhouwer@meertens.knaw.nl>
2015-07-06 16:25:53 +0200

    Some finetuning

--- import/scripts/do-1-convert.sh
+++ import/scripts/do-1-convert.sh
@@ -3,16 +3,13 @@
 date
 
 /import/isle2clarin /import/imdi-to-convert.txt
-ERR=$?
-
-exit $ERR
 
 if [ -d /import/cmd ]; then
 	rm -rf /import/cmd
 fi
 
-mkdir /import/cmd
+mkdir /import/cmd/media-archive
 
-rsync -avzr /import/src/ /import/cmd/ --include '*.cmdi' --exclude '*.imdi' --exclude 'corpman' --exclude 'sessions' --exclude 'mirrored_corpora' --exclude 'media-archive'
+rsync -avzr /import/src/ /import/cmd/media-archibe --include '*.cmdi' --exclude '*.imdi' --exclude 'corpman' --exclude 'sessions' --exclude 'mirrored_corpora' --exclude 'media-archive'
 
 date
