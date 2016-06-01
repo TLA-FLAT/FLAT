36a9be30c695ba5cb8b998553c1c453e13365fa8
danrhe <daniel.vonrhein@gmail.com>
2016-06-01 11:48:33 +0200

    Refactoring and finishing ingest service

---
+++ deposit_ui/Helpers/Ingest_service.php
@@ -0,0 +1,333 @@
+<?php
+/**
+ * Ingest service
+ */
+
+
+/*
+ * load dependencies and settings
+ */
+require_once ($_POST['module_path'] . '/inc/config.inc');
+require_once ($_POST['module_path'] . '/Helpers/Fedora_REST_API.inc');
+
+
+/*
+ * Variable and constant definitions
+ */
+$user_id=$_POST['user_id'];
+$bundle=$_POST['bundle'];
+$collection=$_POST['collection'];
+
+define('SWORD_SCRIPT', $_POST['app_path'] . "/do-sword-upload.sh");
+define('CMD2DC', $_POST['app_path'] . '/cmd2dc.xsl');
+define('LAT2FOX', $_POST['app_path'] . '/lib/lat2fox.jar');
+define('FREEZE_DIR', $_POST['freeze_dir']);
+define('BAG_DIR', $_POST['bag_dir']);
+define('WEB_SERVER', $_POST['web-server']);
+define('BAG_EXE', $_POST['bag_exe']);
+define('FEDORA_HOME', $_POST['fedora_home']);
+putenv ('FEDORA_HOME=' . FEDORA_HOME);
+
+
+/*
+ * function and object definitions
+ */
+
+/**
+ * Recursively removes a directory
+ *
+ * @param string $dir the name of the directory which will be deleted recursively
+ */
+function recursiveRmDir($dir)
+{
+    $iterator = new RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS), \RecursiveIteratorIterator::CHILD_FIRST);
+    foreach ($iterator as $filename => $fileInfo) {
+        if ($fileInfo->isDir()) {
+            rmdir($filename);
+        } else {
+            unlink($filename);
+        }
+    }
+}
+
+/**
+ * Class IngestServiceException is an exception class
+ */
+class IngestServiceException extends Exception {}
+
+
+/**
+ * Class Ingestor 
+ */
+class Ingestor
+{
+
+    public function __construct($data)
+    {
+        $this->entry = $data;
+        $this->backend_bundle_dir = FREEZE_DIR . '/' . $this->entry['user_id'] . '/' . $this->entry['bundle'];
+        $this->ingests = array();
+    }
+
+    public function reset_entry(){
+        $this->entry['status'] = 'awaiting';
+        $this->entry['exceptions'] = NULL;
+        $this->entry['baginplace'] = 0;
+        $this->entry['zipped'] = 0;
+        $this->entry['nfiles'] = NULL;
+        $this->entry['bagged'] = 0;
+        $this->entry['bag_id'] = NULL;
+        $this->entry['foxml'] = 0;
+        $this->entry['bag_ingested'] = 0;
+        $this->entry['data_purged'] = 0;
+        $this->entry['pid_bundle'] = NULL;
+        $this->entry['owner_added'] = NULL;
+        $this->entry['date_bundle_ingest'] = NULL;
+    }
+
+    /**
+     * make bag with data at right location
+     *
+     */
+
+     public function prepareBag()
+    {
+        $command = BAG_EXE . ' baginplace ' . $this->backend_bundle_dir;
+        exec($command, $output, $return);
+        if ($return) {
+            throw new IngestServiceException ('Error making bag');
+        }
+        $command = BAG_EXE . ' update ' . $this->backend_bundle_dir;
+        exec($command, $output, $return);
+        if ($return) {
+            throw new IngestServiceException ('Error updating bag info');
+        } else {
+            $this->entry['baginplace'] = 1;
+        }
+
+    }
+
+    /**
+     * Zips all unhidden files and make bag in bag directory
+     *
+     *
+     */
+    public function zipBag()
+    {
+        $command = "/lat/scripts/zip_sip.sh " . $this->entry['user_id'] . " " . $this->entry["bundle"] . " " . BAG_DIR . " " . $this->backend_bundle_dir;
+        exec($command, $output_prep, $return);
+        if ($return) {
+            throw new IngestServiceException ('Error creating zip file');
+        } else {
+            $this->entry['zipped'] = 1;
+            $this->entry['nfiles'] = intval(str_split($output_prep['0'], 18)[1]);
+        }
+    }
+
+    /**
+     * Create Bag at correct bag location
+     *
+     * @throws IngestServiceException
+     */
+
+    public function doSword(){
+        $command = SWORD_SCRIPT . ' ' . $this->backend_bundle_dir . '/../' . $this->entry['bundle'] . ".zip";
+        exec($command, $output_bag, $return);
+
+        $bag_id = str_split($output_bag['4'], strrpos($output_bag['4'], '/') + 1)[1];
+        
+        // sleep because sword is slow
+        sleep (15);
+        if (!$bag_id || !file_exists(BAG_DIR . '/' . $bag_id)) {
+            throw new IngestServiceException ('Error creating bag. Check bag log (deposit/sword/tmp/' . $bag_id . '/deposit.properties)');
+        } else {
+            $this->entry['bagged'] = 1;
+            $this->entry['bag_id'] = $bag_id;
+        }
+    }
+
+    /**
+     * Change rights of newly created bag directory
+     *
+     * @throws IngestServiceException
+     */
+    public function changeRightsBagDir(){
+        $command = "sudo chmod -R 777 " . BAG_DIR . "/" . $this->entry['bag_id'];
+        exec($command, $output_chmod, $return);
+        if ($return) {
+            throw new IngestServiceException ('Unable to adapt rights of bag directory');
+        }
+    }
+
+    /**
+     *  generates FOXML files from CMDI
+     *
+     * @throws IngestServiceException
+     */
+    public function createFOXML(){
+
+        $command = "java -Xmx4096m -jar " . LAT2FOX . " -d=" . CMD2DC . ' ' . BAG_DIR . '/' . $this->entry['bag_id'];
+        exec($command, $output_fox, $return);
+        if ($return) {
+            throw new IngestServiceException ('Error creating foxml files');
+        } else {
+            $this->entry['foxml'] = 1;
+        }
+    }
+
+    /**
+     * Batch_Ingest for whole bag /SIP
+     *
+     * @throws IngestServiceException
+     */
+
+    public function batchIngest(){
+        $command_b = FEDORA_HOME . "/client/bin/fedora-batch-ingest.sh " . BAG_DIR . "/" . $this->entry['bag_id'] . '/fox ' . BAG_DIR . "/" . $this->entry['bag_id'] . "/log xml info:fedora/fedora-system:FOXML-1.1 localhost:8443 fedoraAdmin fedora https fedora";
+        exec($command_b, $output_ingest, $return);
+        if ($return) {
+            throw new IngestServiceException ('Failed to ingest bundle');
+        } else {
+            // compare number of ingested files with number of frozen files
+            $Ingests = preg_grep("/ingest succeeded for:/", $output_ingest );
+            if (count($Ingests) == $this->entry['nfiles']) {
+                $pid_bundle =preg_grep("/CMD.xml/", $Ingests );
+                $pid_bundle = str_replace("ingest succeeded for: ", "", $pid_bundle);
+                $pid_bundle = str_replace(".xml", "", $pid_bundle);
+                $pid_bundle = str_replace("lat_", "lat:", $pid_bundle);
+                $this->entry['pid_bundle'] = $pid_bundle[0];
+                $this->entry['bag_ingested'] = 1;
+                $this->ingests = $Ingests;
+
+                # delete original
+                recursiveRmDir($this->backend_bundle_dir);
+                rmdir ($this->backend_bundle_dir);
+                if (!file_exists($this->backend_bundle_dir)) $this->entry['data_purged'] = 1;
+
+            } else {
+                throw new IngestServiceException ('Ingest of bundle only partially succeeded');
+            }
+        }
+
+    }
+
+
+    /**
+     *
+     * @param array $Ingests
+     * @throws IngestServiceException
+     */
+    public function changeOwnership($Ingests){
+
+        // create object that can do ReST requests
+        $rest_fedora = new FedoraRESTAPI();
+
+        // Change ownership of ingested files
+        foreach ($Ingests as $f) {
+            $pid = str_replace("ingest succeeded for: ", "", $f);
+            $pid = str_replace(".xml", "", $pid);
+            $pid = str_replace("lat_", "lat:", $pid);
+            $data = array(
+                'ownerId' => $this->entry['user_id']
+            );
+
+            $result = $rest_fedora->modifyObject($pid, $data);
+            if (!$result) {
+                throw new IngestServiceException ('Couldn\'t change ownership of files');
+            } else {
+                $pid_data = $rest_fedora->getObjectData($pid);
+                $this->entry['date_bundle_ingest'] = strtotime($pid_data['objCreateDate']);
+            }
+
+        }
+        $this->entry['owner_added'] = 1;
+        $this->entry['status'] = 'archived';
+    }
+
+    /**
+     * @param database connection handle $db
+     */
+    public function updateDatabase ($db){
+        $conditions= array(
+            "user_id" => $this->entry['user_id'],
+            "bundle" => $this->entry['bundle'],
+        );
+        $res = pg_update($db, 'flat_deposit_ui_upload_log', $this->entry, $conditions);
+        if (!$res) {
+            echo ( "Database table has not been updated\n");
+        }
+    }
+}
+
+
+
+/*
+ * Script part run by every instance
+ */
+
+/*
+ * Uncomment this to create file where time of script execution is logged
+ */
+
+#$file = "/app/flat/deposit/Ingest_service.log";
+#file_get_contents($file);
+#file_put_contents($file, 'Script 2 has slept enough and awakes at ' . date ('D, d M Y H:i:s') . "\n", FILE_APPEND | LOCK_EX);
+
+
+$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$pw";
+$db = pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());;
+
+
+// Performing SQL query to pick up all information of one specific bundle
+$query = sprintf('SELECT * FROM flat_deposit_ui_upload_log WHERE user_id = \'%s\' AND bundle = \'%s\' and collection = \'%s\' AND status = \'awaiting\' AND exceptions = \'\'',$user_id, $bundle, $collection);
+$results = pg_query($db, $query) or die('Query failed: ' . pg_last_error());
+
+
+// Try to ingest one specified row (could be several but selection is now made in commit changes).
+while ($row = pg_fetch_array($results, null, PGSQL_ASSOC)) {
+    try {
+        $ingest = new Ingestor($row);
+        $ingest->entry['status'] = 'being processed';
+        $ingest->updateDatabase($db);
+
+        $ingest->prepareBag();
+        $ingest->zipBag();
+
+        $ingest->doSword();
+
+        $ingest->changeRightsBagDir();
+        $ingest->createFOXML();
+        $ingest->batchIngest();
+
+        $ingest->changeOwnership($ingest->ingests);
+
+    } catch(IngestServiceException $e){
+        $ingest->reset_entry();
+        $ingest->entry['status'] = 'failed';
+        $ingest->entry['exceptions'] = $e->getMessage();
+        print_r($ingest->entry['exceptions']);
+
+    } finally {
+        // Cleanup: bagit files, zip file and bag
+        array_map('unlink', glob($ingest->backend_bundle_dir . "/*.txt"));
+        if (file_exists(FREEZE_DIR . '/' . $ingest->entry['user_id'] . '/' . $ingest->entry['bundle'] . ".zip")) unlink(FREEZE_DIR . '/' . $ingest->entry['user_id'] . '/' . $ingest->entry['bundle'] . ".zip");
+        if (file_exists(BAG_DIR . '/' . $ingest->entry['bag_id']) && $ingest->entry['bag_id']) {recursiveRmDir(BAG_DIR . '/' . $ingest->entry['bag_id']);
+            rmdir(BAG_DIR . '/' . $ingest->entry['bag_id']);}
+        // update entry on database
+        $ingest->updateDatabase($db);
+    }
+
+
+    }
+
+
+// Free resultset
+pg_free_result($results);
+
+
+pg_close($db);
+
+
+#file_put_contents($file, "Done\n", FILE_APPEND | LOCK_EX);
+
+
+
