e30ec76452585738b5bebe5f4379c1786c0ef648
danrhe <daniel.vonrhein@gmail.com>
2017-02-13 17:35:49 +0100

    added flat_collection content_type

--- docker/add-deposit-ui-to-flat/flat_deposit/inc/drupal_queries.php
+++ docker/add-deposit-ui-to-flat/flat_deposit/inc/drupal_queries.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * Drupal query of user collections.
+ * Drupal query of user supercollections.
  *
- * @param $pid array of strings containing collections to be queried
+ * @param $pid array of strings containing supercollections to be queried
  * @param $transformed bool put data in flat structure
  *
  * @return mixed array of collection names and pids
  */
-function query_user_collections($pid=NULL,$transformed=FALSE)
+function query_user_supercollections($pid=NULL,$transformed=FALSE)
 {
     global $user;
-    $query = db_select('flat_collection', 'p')
+    $query = db_select('flat_supercollection', 'p')
         ->fields('p', array('collection_name', 'collection_pid',))
         ->condition('member', $user->name)
         ->orderBy('collection_name');
@@ -21,18 +21,18 @@ function query_user_collections($pid=NULL,$transformed=FALSE)
     $results =$query->execute()
         ->fetchAll(PDO::FETCH_ASSOC);
 
-    $collections = array();
+    $supercollections = array();
 
     if ($transformed){
         foreach ($results as $result){
-            $collections['collection_name'][]=$result['collection_name'];
-            $collections['collection_pid'][]=$result['collection_pid'];
+            $supercollections['collection_name'][]=$result['collection_name'];
+            $supercollections['collection_pid'][]=$result['collection_pid'];
         }
     } else {
-        $collections = $results;
+        $supercollections = $results;
     }
 
-    return $collections;
+    return $supercollections;
 }
 
 
