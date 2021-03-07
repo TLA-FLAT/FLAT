002c5aab66179fa1646c06fc1c5f106e7a4fbd84
Menzo Windhouwer <menzo@windhouwer.nl>
2021-03-07 12:21:31 +0100

    enable HTTPS & add FLAT_START_MODE
    use
    - FLAT_START_MODE=fg to get an interactive bash shell
    - FLAT_START_MODE=bg for docker compose

--- docker/flat/supervisor/start.sh
+++ docker/flat/supervisor/start.sh
@@ -4,7 +4,21 @@ service supervisor start &&\
 /wait-postgres.sh &&\
 supervisorctl start tomcat &&\
 /wait-tomcat.sh
+ERR="${?}"
+if [ ! ${ERR} = 0 ]; then
+    exit ${ERR}
+fi
 
 #http://patorjk.com/software/taag/#p=display&f=Slant%20Relief&t=FLAT
-cat /flat.txt &&\
-tail -f /dev/null
+cat /flat.txt 
+
+#docker: /bin/bash -l $*
+#compose: tail -f /dev/null
+
+if [ ${FLAT_START_MODE} = 'bg' ]; then
+    echo "flat start mode[bg]"
+    tail -f /dev/null
+else
+    echo "flat start mode[fg]"
+    /bin/bash -l $*
+fi
